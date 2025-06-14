242cd1525ff5d637cf8987dff526d23d98f5fe68
Wesley Leite <leitewesleyleite@gmail.com>
2025-06-14 15:51:09 -0300

    Dtos adjust

--- FastTechFoods.Orders.Application/Dtos/OrderPostDto.cs
+++ FastTechFoods.Orders.Application/Dtos/OrderPostDto.cs
@@ -1,30 +1,34 @@
 using System.ComponentModel.DataAnnotations;
-using FastTechFoods.Orders.Domain.Entities;
 using FastTechFoods.Orders.Domain.Enums;
 
 namespace FastTechFoods.Orders.Application.Dtos
 {
-    public class OrderPostDto
+    public class OrderDto
     {
-        [Required(ErrorMessage = "Id User is required")]
-        public Guid IdUser { get; set; }
-
         [Required(ErrorMessage = "Id Store is required")]
         public Guid IdStore { get; set; }
 
+        [Required(ErrorMessage = "Id User is required")]
+        public Guid IdUser { get; set; }
+                
         [Required(ErrorMessage = "Delivery Type is required")]
         public DeliveryType DeliveryType { get; set; }
 
         [Required(ErrorMessage = "Order Item is required")]
-        public List<OrderItemsDto> OrderItems { get; set; }
+        public required IEnumerable<ItemDto> Items { get; set; }
     }
 
-    public class OrderItemsDto
+#nullable disable
+    public class ItemDto
     {
+        public Guid Id { get; set; }
         public Guid MenuItemId { get; set; }
-        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
-
+        public decimal Amount { get; set; }
+        public Category Category { get; set; }
         public string Notes { get; set; }
     }
+#nullable restore
 }
