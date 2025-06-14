99db8f84f75f1804f85bd48afcb7c938a1dd24bd
Wesley Leite <leitewesleyleite@gmail.com>
2025-06-14 20:56:58 -0300

    Justification

--- FastTechFoods.Orders.Application/Dtos/ChangeStatusDto.cs
+++ FastTechFoods.Orders.Application/Dtos/ChangeStatusDto.cs
@@ -3,5 +3,6 @@ namespace FastTechFoods.Orders.Application.Dtos
     public class ChangeStatusDto
     {
         public Guid OrderId { get; set; }
+        public string Justification { get; set; }
     }
 }
